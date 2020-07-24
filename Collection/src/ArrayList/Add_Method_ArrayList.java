87befbde9c08908a9745ef41fb4ae8a8219c967a
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-07-24 22:25:58 +0530

    Add

---
+++ Collection/src/ArrayList/Add_Method_ArrayList.java
@@ -0,0 +1,18 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Add_Method_ArrayList {
+	public static void main(String[] args) {
+		//ArrayList<String> animal = new ArrayList<String>();
+		//or
+		ArrayList<String> animal = new ArrayList<String>();
+		
+		animal.add("Dog");
+		animal.add("Cow");
+		animal.add("Elephant");
+		System.out.println(animal);
+		
+	}
+
+}//op:-[Dog, Cow, Elephant]
