7f143d2e302c846f2bfee3e7c386b6c3ac05af74
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-03 22:46:22 +0530

    Implementation String

---
+++ String_Handling/src/String_Implementation/Implementation.java
@@ -0,0 +1,27 @@
+package String_Implementation;
+
+public class Implementation {
+	public static void main(String[] args) {
+		
+		//Creating String By String Literal
+		String s1 = "Java";
+		System.out.println(s1);
+		
+		//op:-string
+		char ch[] = {'s','t','r','i','n','g'};
+		System.out.println(ch);
+		
+		//converting char array to string  
+		String s2 = new String(ch);
+		System.out.println(s2);
+		
+		
+		//creating java string by new keyword
+		String s3 = new String("Example");
+		System.out.println(s3);
+		//op:-example
+		
+		
+	}
+
+}
