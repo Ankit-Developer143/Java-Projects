b913fac8695021d3efde145fe0f54befed98f87d
Ankit Singh <59916393+Ankit-Developer143@users.noreply.github.com>
2020-08-04 22:21:08 +0530

    immutable String

--- String_Handling/src/All_types/Immuttable_String.java
+++ String_Handling/src/All_types/Immuttable_String.java
@@ -1,5 +1,21 @@
-package String_Implementation;
+package All_types;
 
 public class Immuttable_String {
+	public static void main(String[] args) {
+		String s = "Sachin";
+		/*
+		 * As you can see in the above figure that two objects are created but s
+		 * reference variable still refers to "Sachin" not to "Sachin Tendulkar". But if
+		 * we explicitely assign it to the reference variable, it will refer to
+		 * "Sachin Tendulkar" object.For example:
+		 */
+		// String result = s.concat("ankit");
+		// System.out.println(result);
+
+		// Immutable
+		s.concat("Ankit");
+		System.out.println(s);
+		// op:-Sachin
+	}
 
 }
